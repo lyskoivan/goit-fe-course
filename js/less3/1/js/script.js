c2c0852107c8e63fe154794193877dacb8b7a1f2
lyskoivan <lysko1999i@gmail.com>
2019-08-08 22:31:24 +0300

    hw 3 end

--- js/less3/1/js/script.js
+++ js/less3/1/js/script.js
@@ -1,14 +1,30 @@
 'use strict'
-const newArray = [];
-const fn = function (elements, callback) {
-    for (const element of elements) {
-        const result = callback(element);
-        newArray.push(result);
-    }
-    return newArray;
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const isLoginValid = function(login) {
+  if (login.length < 4 || login.length > 16) {
+    return false;
+  }
 };
-const sortOut = function (num) {
-    return num * 2;
+const isLoginUnique = function(allLogins, login) {
+  return allLogins.includes(login);
 };
-const array = [1,2,3,4,5];
-console.log(fn(array, sortOut));
+
+const addLogin = function(allLogins, login) {
+  if (isLoginValid(login) === false) {
+    console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+    return;
+  }
+  if (isLoginUnique(allLogins, login) === true) {
+    return console.log('Такой логин уже используется!');;
+  }
+  else {
+    allLogins.push(login);
+    return console.log('Логин успешно добавлен!');;
+  }
+};
+
+// Вызовы функции для проверки
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
